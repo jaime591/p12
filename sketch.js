5ae722842b66de144b88279c03efaf2624a0a558
jaime591 <138538470+jaime591@users.noreply.github.com>
2023-07-12 21:41:01 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,114 @@
-var ship,sea,ship_running;
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+
+
 function preload(){
-  ship_running=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
-  seaImg=loadImage("sea.png");
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
 }
+
+
 function setup(){
-  createCanvas(600,600);
-  sea=createSprite(0,150,10,10);
-  sea.addImage(seaImg);
-  sea.velocityX=5
-  ship=createSprite(150,400,50,50);
-  ship.addAnimation("running",ship_running);
-  ship.scale=0.5
-  sea.x = sea.width /2;
-  sea.velocityX = -4;
+  
+  createCanvas(400,400);
+//Mover fondo
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+
+//crear sprite rabbit 
+rabbit = createSprite(160,340,20,20);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
 }
+
 function draw() {
-  background("blue");
-  if(sea.x<0){
-    sea.x=sea.width/2;
-  }
- drawSprites();
+  background(0);
+  
+  //mover el sprite rabbit en el eje X con el mouse
+  rabbit.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+  
+   drawSprites();
+   
+  
+ var select_sprites = Math.random(random(1,3));
+
+ var select_sprites = Math.random(random(1,3));
+
+ var select_sprites = Math.round(1,3);
+
+ var select_sprites = Math.round(random(1,3));
+
+  
+   if (frameCount % 50 == 0) {
+   if (select_sprites == 1) {
+     createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }else {
+       createRed();
+     }
+   }
+
+   if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }
+   }
+
+   if (frameCount / 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }else {
+       createRed();
+     }
+   }
+
+   if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }else {
+       createRed();
+     }
+   }
+
+
+
+}
+
+function createApples() {
+apple = createSprite(random(50, 350),40, 10, 10);
+apple.addImage(appleImg);
+apple.scale=0.07;
+apple.velocityY = 3;
+apple.lifetime = 150;
+  
+}
+
+function createOrange() {
+orangeL = createSprite(random(50, 350),40, 10, 10);
+orangeL.addImage(orangeImg);
+orangeL.scale=0.08;
+orangeL.velocityY = 3;
+orangeL.lifetime = 150;
+}
+
+function createRed() {
+redL = createSprite(random(50, 350),40, 10, 10);
+redL.addImage(redImg);
+redL.scale=0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 150;
 }
